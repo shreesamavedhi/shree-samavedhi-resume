6c7731422bab0eddaf4dcd7217a4613f3d2d45cd
shreesamavedhi <shreesamavedhi114@gmail.com>
2020-09-06 23:47:35 -0500

    Updates

---
+++ precache-manifest.0fc4c8f3c4936d4a5463be65624af744.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb9520f582a037689bf5d8935a2fac0e",
+    "url": "/shree-resume/index.html"
+  },
+  {
+    "revision": "3aca19eb1b7a4c456286",
+    "url": "/shree-resume/static/css/main.f169819d.chunk.css"
+  },
+  {
+    "revision": "afa936b7873edbf8cf24",
+    "url": "/shree-resume/static/js/2.ba5513a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shree-resume/static/js/2.ba5513a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aca19eb1b7a4c456286",
+    "url": "/shree-resume/static/js/main.2c33b480.chunk.js"
+  },
+  {
+    "revision": "bcbbc12f27c1dabb4323",
+    "url": "/shree-resume/static/js/runtime-main.6f57e6f3.js"
+  },
+  {
+    "revision": "b48998392367fafcfa545832811d1cb8",
+    "url": "/shree-resume/static/media/profile.b4899839.jpg"
+  }
+]);
